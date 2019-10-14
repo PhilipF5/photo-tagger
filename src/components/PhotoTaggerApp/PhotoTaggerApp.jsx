2e3720b14c0ace8e0109e734b0ef9a8c2f4f4dce
Philip Fulgham <philip@philipf5.dev>
2019-10-14 16:18:25 -0400

    Get all files listed in folder as images

--- src/components/PhotoTaggerApp/PhotoTaggerApp.jsx
+++ src/components/PhotoTaggerApp/PhotoTaggerApp.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { getFilesFromFolder, loadFile } from "../../utilities/file-utilities";
 import Gallery from "../Gallery/Gallery";
 import styles from "./PhotoTaggerApp.module.css";
 
@@ -7,6 +8,20 @@ const { remote } = window.require("electron");
 const PhotoTaggerApp = () => {
 	const [images, setImages] = useState([]);
 	const [selectedTags, setSelectedTags] = useState([]);
+
+	useEffect(() => {
+		remote.dialog.showOpenDialog(
+			remote.getCurrentWindow(),
+			{ properties: ["openDirectory"] },
+			async ([folderPath]) => {
+				const fileNames = await getFilesFromFolder(folderPath);
+				const files = await Promise.all(fileNames.map(async (fn) => await loadFile(`${folderPath}/${fn}`)));
+				console.log(files);
+				setImages(files);
+			},
+		);
+	}, []);
+
 	return (
 		<div className={styles.app}>
 			<Gallery images={images} setImages={setImages} selectedTags={selectedTags} />
--- src/utilities/file-utilities.ts
+++ src/utilities/file-utilities.ts
@@ -2,6 +2,11 @@ import { XmpSidecar } from "./xmp-sidecar";
 const fs = window.require("fs").promises;
 const path = window.require("path");
 
+export const getFilesFromFolder = async (folderPath: string) => {
+	const files = await fs.readdir(folderPath);
+	return files.filter((f: string) => !f.startsWith(".") && path.extname(f) !== ".xmp");
+};
+
 export const loadFile = async (filePath: string) => {
 	return {
 		content: (await fs.readFile(filePath)).toString("base64"),
