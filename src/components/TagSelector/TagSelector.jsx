00ad186dedaac2fff72db857e5b3f43da0cede7c
Philip Fulgham <philip@philipf5.dev>
2019-10-14 20:33:58 -0400

    Add ability to create and store tags

--- src/components/TagSelector/TagSelector.jsx
+++ src/components/TagSelector/TagSelector.jsx
@@ -2,15 +2,26 @@ import React, { useEffect, useState } from "react";
 import Tag from "../Tag/Tag";
 import styles from "./TagSelector.module.css";
 
-const Store = require("electron-store");
+const Store = window.require("electron-store");
 const store = new Store({ name: "tagList" });
 
 const TagSelector = ({ selectedTags, setSelectedTags }) => {
 	const [tags, setTags] = useState(store.get("tags") || []);
+	const [newTag, setNewTag] = useState("");
 
 	useEffect(() => {
 		store.set("tags", tags);
 	}, [tags]);
+
+	const addTag = () => !tags.includes(newTag) && setTags([...tags, newTag]);
+	const handleNewTagChange = ({ target: { value } }) => setNewTag(value);
+	const toggleTag = (tag) => {
+		if (selectedTags.includes(tag)) {
+			setSelectedTags(selectedTags.filter((t) => t !== tag));
+		} else {
+			setSelectedTags([...selectedTags, tag]);
+		}
+	};
 
 	return (
 		<div className={styles.sidebar}>
@@ -20,6 +31,20 @@ const TagSelector = ({ selectedTags, setSelectedTags }) => {
 			))}
 			<h3>Suggested tags</h3>
 			<h3>All tags</h3>
+			<div className={styles.tag}>
+				<input type="text" value={newTag} onChange={handleNewTagChange} placeholder="Add a tag..." />
+				<button className={styles.addButton} onClick={addTag}>
+					+
+				</button>
+			</div>
+			{tags.sort().map((t) => (
+				<div className={styles.tag}>
+					<div className={styles.tagname} onClick={() => toggleTag(t)}>
+						{t}
+					</div>
+					<button className={styles.deleteButton}>X</button>
+				</div>
+			))}
 		</div>
 	);
 };
