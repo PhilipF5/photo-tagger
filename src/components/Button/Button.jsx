becc6b73b2b8ced37dc7620b45bd1f730b3aa396
Philip Fulgham <philip@philipf5.dev>
2019-10-16 09:57:18 -0400

    Add disabled state for Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,9 +2,9 @@ import classNames from "classnames";
 import React from "react";
 import styles from "./Button.module.css";
 
-const Button = ({ children, className, onClick }) => {
+const Button = ({ children, className, disabled, onClick }) => {
 	return (
-		<button className={classNames(styles.button, className)} onClick={onClick}>
+		<button className={classNames(styles.button, className, { [styles.disabled]: disabled })} onClick={onClick}>
 			<div className={styles.background}></div>
 			{children}
 		</button>
