7e72e2ad95c196948cd9e01a7dd57aa41331cf69
Philip Fulgham <philip@philipf5.dev>
2019-10-15 20:45:19 -0400

    Use function syntax in TS files

--- src/utilities/file-utilities.ts
+++ src/utilities/file-utilities.ts
@@ -1,14 +1,14 @@
 const fs = window.require("fs").promises;
 const path = window.require("path");
 
-export const getFilesFromFolder = async (folderPath: string) => {
+export async function getFilesFromFolder(folderPath: string): Promise<string[]> {
 	const files = await fs.readdir(folderPath);
 	return files.filter((f: string) => !f.startsWith(".") && path.extname(f) !== ".xmp");
-};
+}
 
-export const loadFile = async (filePath: string) => {
+export async function loadFile(filePath: string) {
 	return {
 		content: (await fs.readFile(filePath)).toString("base64"),
 		path: path.parse(filePath),
 	};
-};
+}
