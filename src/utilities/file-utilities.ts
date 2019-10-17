787a434a895a47e9e27d9fd6a41d3375d72a58d3
Philip Fulgham <philip@philipf5.dev>
2019-10-17 11:04:33 -0400

    Add generic file-saving functionality

--- src/utilities/file-utilities.ts
+++ src/utilities/file-utilities.ts
@@ -1,12 +1,14 @@
-const fs = window.require("fs").promises;
-const path = window.require("path");
+import fs from "fs";
+import path from "path";
+const _fs: typeof fs.promises = window.require("fs").promises;
+const _path: typeof path = window.require("path");
 
 const excludedExts = [".xmp", ".db", ".mp4"];
 
 export async function getFilesFromFolder(folderPath: string): Promise<string[]> {
-	const files = await fs.readdir(folderPath);
+	const files = await _fs.readdir(folderPath);
 	return files
-		.filter((f: string) => !f.startsWith(".") && !excludedExts.includes(path.extname(f)))
+		.filter((f: string) => !f.startsWith(".") && !excludedExts.includes(_path.extname(f)))
 		.map((f: string) => `${folderPath}/${f}`);
 }
 
@@ -16,7 +18,11 @@ export function getPageOfFiles(files: string[], page: number, pageSize: number) 
 
 export async function loadFile(filePath: string, encoding: string = "utf8") {
 	return {
-		content: (await fs.readFile(filePath)).toString(encoding),
-		path: path.parse(filePath),
+		content: (await _fs.readFile(filePath)).toString(encoding),
+		path: _path.parse(filePath),
 	};
+}
+
+export function saveFile(filePath: string, contents: string) {
+	return _fs.writeFile(filePath, contents);
 }
